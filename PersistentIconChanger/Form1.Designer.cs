f20ba6ba79c59ae4857638d3d1722563f9046296
Endianteque <matteoperin1302@gmail.com>
2016-09-24 13:40:20 +0200

    Project Files

---
+++ PersistentIconChanger/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace PersistentIconChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedFolderBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectedImageBox = new System.Windows.Forms.TextBox();
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.selectImageDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(814, 13);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(28, 22);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "...";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selected Folder:";
+            // 
+            // selectedFolderBox
+            // 
+            this.selectedFolderBox.Location = new System.Drawing.Point(102, 14);
+            this.selectedFolderBox.Name = "selectedFolderBox";
+            this.selectedFolderBox.ReadOnly = true;
+            this.selectedFolderBox.Size = new System.Drawing.Size(706, 20);
+            this.selectedFolderBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Selected Icon:";
+            // 
+            // selectedImageBox
+            // 
+            this.selectedImageBox.Location = new System.Drawing.Point(102, 48);
+            this.selectedImageBox.Name = "selectedImageBox";
+            this.selectedImageBox.ReadOnly = true;
+            this.selectedImageBox.Size = new System.Drawing.Size(706, 20);
+            this.selectedImageBox.TabIndex = 4;
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Location = new System.Drawing.Point(814, 47);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(28, 22);
+            this.selectImageButton.TabIndex = 5;
+            this.selectImageButton.Text = "...";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.selectImageButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Icon Preview:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "256x256";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Enabled = false;
+            this.changeButton.Location = new System.Drawing.Point(15, 385);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(576, 23);
+            this.changeButton.TabIndex = 9;
+            this.changeButton.Text = "Change Icon";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(385, 214);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(540, 278);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.InitialImage = null;
+            this.pictureBox4.Location = new System.Drawing.Point(631, 294);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.InitialImage = null;
+            this.pictureBox5.Location = new System.Drawing.Point(706, 310);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.InitialImage = null;
+            this.pictureBox6.Location = new System.Drawing.Point(765, 318);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.InitialImage = null;
+            this.pictureBox7.Location = new System.Drawing.Point(816, 326);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox7.TabIndex = 16;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(425, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "128x128";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "64x64";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(638, 345);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "48x48";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(705, 345);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "32x32";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(760, 345);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "24x24";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(806, 345);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "16x16";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Enabled = false;
+            this.resetButton.Location = new System.Drawing.Point(604, 385);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(238, 23);
+            this.resetButton.TabIndex = 23;
+            this.resetButton.Text = "Restore Default";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 420);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.selectedImageBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectedFolderBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectFolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Persistent Icon Changer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox selectedFolderBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox selectedImageBox;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.OpenFileDialog selectImageDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ PersistentIconChanger/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PersistentIconChanger
+{
+    public partial class Form1 : Form
+    {
+        private Image selectedImage;
+        private string selectedFolder;
+        private string selectedImageName;
+        private string selectedImagePath;
+        private string iniPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Setting openFileDialogFilters
+            selectImageDialog.Filter = "Icon Files (*.ico)| *.ico";
+            //Inizialization of icon preview picture boxes
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            updateImageBoxes(new Bitmap(Properties.Resources.folder));
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFolderBox.Text = folderBrowserDialog.SelectedPath;
+                selectedFolder = folderBrowserDialog.SelectedPath;
+                if (selectedImageBox.Text != "")    //Activate change icon button if image is already selected
+                    changeButton.Enabled = true;
+                resetButton.Enabled = true;         //Activate reset button as soon as a folder is selected
+                iniPath = selectedFolder + "\\desktop.ini";
+            }
+        }
+
+        private void selectImageButton_Click(object sender, EventArgs e)
+        {
+            Stream fileStream = null;
+            if (selectImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((fileStream = selectImageDialog.OpenFile()) != null)
+                        using (fileStream)
+                            selectedImage = Image.FromFile(selectImageDialog.FileName);
+                    selectedImageName = selectImageDialog.SafeFileName;     //Get file "name.ico"
+                    selectedImagePath = selectImageDialog.FileName;
+                    updateImageBoxes(selectedImage);
+                    selectedImageBox.Text = selectImageDialog.FileName;
+                    if (selectedFolderBox.Text != "")                       //Activate change icon button if folder is already selected
+                        changeButton.Enabled = true;
+                }
+                //Exception management
+                catch (OutOfMemoryException)
+                {
+                    MessageBox.Show("Invalid image format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("The file does not exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("Filename is a Uri", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not read file from disk: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void updateImageBoxes(Image image)
+        {
+            //Set the same image for each picture box
+            pictureBox1.Image = image;
+            pictureBox2.Image = image;
+            pictureBox3.Image = image;
+            pictureBox4.Image = image;
+            pictureBox5.Image = image;
+            pictureBox6.Image = image;
+            pictureBox7.Image = image;
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Erase old icons
+                eraseOldIcons();
+
+                //Create an hidden copy of selected icon file
+                string copyPath = selectedFolder + "\\" + selectedImageName;
+                //If the same icon already exists, set its attributes to normal in order to be able to overwrite it
+                if (File.Exists(copyPath))
+                    File.SetAttributes(copyPath, FileAttributes.Normal);
+                //Copy the icon, ovewriting if necessary
+                File.Copy(selectedImagePath, copyPath, true);
+                //Set icon file attributes back to hidden
+                File.SetAttributes(copyPath, FileAttributes.Hidden);
+
+                //Set the folder attributes to System (necessary for telling windows to refresh icon.db cache)
+                File.SetAttributes(selectedFolder, FileAttributes.System);
+
+                //.ini file creation
+                string text = getIniContent();
+                string iniPath = selectedFolder + "\\desktop.ini";
+                //If desktop.ini already exists, set its attributes to normal in order to be able to overwrite it
+                if (File.Exists(iniPath))
+                {
+                    File.SetAttributes(iniPath, FileAttributes.Normal);
+                    File.Delete(iniPath);   //Delete old desktop.ini to avoid conflict
+                }
+                File.WriteAllText(iniPath, text);
+                //Set desktop.ini attributes back to hidden, system and to read-only for security
+                File.SetAttributes(iniPath, FileAttributes.Hidden | FileAttributes.ReadOnly | FileAttributes.System);
+
+                MessageBox.Show("Icon successfully changed!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error changing the icon: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Look for an hidden file in the same path where desktop.ini is saved (File.Exists() does not return true for hidden files)
+                if ((new FileInfo(iniPath).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    File.SetAttributes(iniPath, FileAttributes.Normal); //Set desktop.ini attributes to normal to allow deletion
+                    File.Delete(iniPath);   //Delete desktop.ini to restore the default folder icon
+                    eraseOldIcons();
+                    MessageBox.Show("Icon successfully restored!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(FileNotFoundException)    //If destop.ini file is not found, there is no custom icon set
+            {
+                MessageBox.Show("The folder icon is already the default one", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error restoring default icon: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string getIniContent()
+        {
+            /* Returns the content necessary for windows to display the icon, with the following structure:
+             * [.ShellClassInfo]
+             * IconResource="fileName".ico, 0
+             * [ViewState]
+             * Mode =
+             * Vid =
+             * FolderType = Generic
+             */
+             return "[.ShellClassInfo]\nIconResource=" + selectedImageName + ",0\n[ViewState]\nMode =\nVid =\nFolderType = Generic";
+        }
+
+        private void eraseOldIcons()
+        {
+            //Delete all hidden .ico files in the selected folder
+            string[] files = Directory.GetFiles(selectedFolder);
+            foreach(string s in files)
+            {
+                if ((new FileInfo(s).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden && Path.GetExtension(s).Equals(".ico"))
+                {
+                    File.SetAttributes(s, FileAttributes.Normal);
+                    File.Delete(s);
+                    Console.WriteLine(s + " deleted.");
+                }
+            }
+        }
+    }
+}
